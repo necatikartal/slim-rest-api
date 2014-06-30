57102bb6f648e8bca31fe5145f87172d044b7d59
necatikartal <nctkrtl@hotmail.com>
2014-06-30 16:40:06 +0300

    database configuration file added

---
+++ library/config.php
@@ -0,0 +1,10 @@
+<?php
+	define('DB_USERNAME', 'root');
+	define('DB_PASSWORD', 'nctkrtl13');
+	define('DB_HOST', 'localhost');
+	define('DB_NAME', 'SlimRESTDb');
+
+	define('USER_CREATED_SUCCESSFULLY', 0);
+	define('USER_CREATE_FAILED', 1);
+	define('USER_ALREADY_EXISTED', 2);
+?>
