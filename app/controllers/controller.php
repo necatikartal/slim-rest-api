767baf884e317934d6593483a8b8e6de1bdd9124
necatikartal <nctkrtl@hotmail.com>
2014-06-30 16:35:35 +0300

    updated controller.php

--- app/controllers/controller.php
+++ app/controllers/controller.php
@@ -2,41 +2,35 @@
 
 class controller {
 
-	public function __construct() 
-	{
-
+	# Define default construct
+	public function __construct() {
 	}
 
 	# Define a GET Method Controller
-	public function get()
-	{
+	public function get($id = null) {
 		include "../app/models/user.php";
 		$m = new user;
-		$m -> select();
+		$m -> select($id);
 	}
 
 	# Define a POST Method Controller
-	public function post()
-	{
+	public function post() {
 		include "../app/models/user.php";
 		$m = new user;
 		$m -> insert();
 	}
 
 	# Define a PUT Method Controller
-	public function put()
-	{
+	public function put($id) {
 		include "../app/models/user.php";
 		$m = new user;
-		$m -> update();
+		$m -> update($id);
 	}
 
 	# Define a DELETE Method Controller
-	public function delete()
-	{
+	public function delete($id) {
 		include "../app/models/user.php";
 		$m = new user;
-		$m -> delete();
+		$m -> delete($id);
 	}
-
 }
