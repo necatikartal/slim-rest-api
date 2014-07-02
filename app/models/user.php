297ecfce0991cc7f02e5b278c8afc6b3d7266124
necatikartal <nctkrtl@hotmail.com>
2014-07-02 10:02:07 +0300

    used pdo methods to exec queries

--- app/models/user.php
+++ app/models/user.php
@@ -17,39 +17,37 @@ class User {
 
 		$cmd = "SELECT id, username, userpassword FROM users";
 		$cmd.= ($id == null) ? "" :  " WHERE id=" . $id ;
-		
-		$result = mysql_query( $cmd, $this->resource); 
+
+		$this->resource->query($cmd);
 
 		$resultSet = array();
-			while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
-				$tmp = array(
+		foreach($this->resource->query($cmd) as $row) {
+			$tmp = array(
 					'id' => $row["id"], 
 					'username' => $row["username"], 
 					"userpassword" => $row["userpassword"]);
 				array_push($resultSet, $tmp);
 			}
+		
 		echo json_encode($resultSet);
-		mysql_free_result($result);
 	}
 
 	# Define a INSERT Method 
 	public function insert($data){
-		mysql_query("INSERT INTO users ( username , userpassword) 
-			VALUES ('". $data.username ."','". $data.userpassword . "')"
-			, $this->resource);
+		$this->resource->prepare("INSERT INTO users ( username , userpassword) VALUES (?,?)");
+		$this->resource->execute(array($data['username'], $data['userpassword']));
 	}
 
 	# Define a UPDATE Method
 	public function update($id , $data){
-
-		mysql_query("UPDATE users SET userpassword=" . $data.userpassword . "WHERE id=" . $id 
-			, $this->resource);
+		$cmd = "UPDATE users SET userpassword=" . $data['userpassword'] . "WHERE id=" . $id;
+		$this->resource->exec($cmd);
 	}
 
 	# Define a DELETE Method
 	public function delete($id){
-		mysql_query("DELETE FROM users WHERE id=" . $id 
-			, $this->resource);
+		$cmd = "DELETE FROM users WHERE id=" . $id;
+		$this->resource->exec($cmd);
 	}
 
 }
