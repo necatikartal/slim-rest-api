1ad18e3b6fd98aca75127aa24544049f9e408aaa
necatikartal <nctkrtl@hotmail.com>
2014-07-02 09:05:55 +0300

    sql commands added to insert and update methods

--- app/models/user.php
+++ app/models/user.php
@@ -15,50 +15,41 @@ class User {
 	# Define a SELECT Method 
 	public function select($id = null) {
 
-		if($id == null)
-		{
-			$result = mysql_query("SELECT id, username, userpassword FROM users", $this->resource);
-
-			$resultSet = array();
-			while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
-				$tmp = array(
-					'ID' => $row["id"], 
-					'Username' => $row["username"], 
-					"Userpassword" => $row["userpassword"]);
-				array_push($resultSet, $tmp);
-			}
-		}
-		else
-		{
-			$result = mysql_query("SELECT id, username, userpassword FROM users WHERE id =" . $id , $this->resource);
-
-			$resultSet = array();
-			while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
-				$tmp = array(
-					'ID' => $row["id"], 
-					'Username' => $row["username"], 
-					"Userpassword" => $row["userpassword"]);
-				array_push($resultSet, $tmp);
-			}
-		}
+		$cmd = "SELECT id, username, userpassword FROM users";
+		$cmd.= ($id == null) ? "" :  " WHERE id=" . $id ;
 		
+		$result = mysql_query( $cmd, $this->resource); 
+
+		$resultSet = array();
+			while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+				$tmp = array(
+					'id' => $row["id"], 
+					'username' => $row["username"], 
+					"userpassword" => $row["userpassword"]);
+				array_push($resultSet, $tmp);
+			}
 		echo json_encode($resultSet);
 		mysql_free_result($result);
 	}
 
 	# Define a INSERT Method 
-	public function insert(){
-
+	public function insert($data){
+		mysql_query("INSERT INTO users ( username , userpassword) 
+			VALUES ('". $data.username ."','". $data.userpassword . "')"
+			, $this->resource);
 	}
 
 	# Define a UPDATE Method
-	public function update($id){
+	public function update($id , $data){
 
+		mysql_query("UPDATE users SET userpassword=" . $data.userpassword . "WHERE id=" . $id 
+			, $this->resource);
 	}
 
 	# Define a DELETE Method
 	public function delete($id){
-
+		mysql_query("DELETE FROM users WHERE id=" . $id 
+			, $this->resource);
 	}
 
 }
