6580ad028aa4404ba4cc1ad73e6d77397bfbc02c
necatikartal <nctkrtl@hotmail.com>
2014-07-04 16:08:38 +0300

    redesigned user model

--- app/models/user.php
+++ app/models/user.php
@@ -2,69 +2,40 @@
 
 class User {
 
-	private $link;
-	private $resource; 
-	private $queryBuilder;
+	private $id;
+	private $username;
+	private $userpassword;
 
-	# Define default construct
 	public function __construct() {
-		include "../library/dbconnect.php";
-		$this->link = new dbconnect;
-		$this->resource = $this->link->connect();
-		$this->queryBuilder = $this->resource->createQueryBuilder();
 	}
 
-	# Define a get user method 
-	public function getUser($id) {
-		$this->queryBuilder
-				->select('u.id','u.username','u.userpassword')
-				->from('users', 'u')
-				->where('u.id = ?')
-				->setParameter(0, $id);
-
-		$result = $this->resource->query($this->queryBuilder);
-		$resultSet = $result->fetchAll();
-		echo json_encode($resultSet);
+	public function setId($id) {
+		$this->id = $id;
 	}
 
-	# Define a get users method 
-	public function getUsers() {
-		$this->queryBuilder
-				->select('u.id','u.username','u.userpassword')
-				->from('users', 'u');
-		
-		$result = $this->resource->query($this->queryBuilder);
-		$resultSet = $result->fetchAll();
-		echo json_encode($resultSet);
+	public function getId() {
+		return $this->id;
 	}
 
-	# Define a order users method
-	public function orderUsers($order, $dir) {
-		$this->queryBuilder
-				->select('u.id','u.username','u.userpassword')
-				->from('users', 'u')
-				->orderBy('?', '?')
-				->setParameter(0, 'u'. $order)
-				->setParameter(1, $dir);
-
-		$result = $this->resource->query($this->queryBuilder);
-		$resultSet = $result->fetchAll();
-		echo json_encode($resultSet);
+	public function setUsername($username) {
+		$this->username = $username;
 	}
 
-	# Define a INSERT Method 
-	public function insertUser($data){
-		$this->resource->insert('users', array('username' => $data['username'], 'userpassword' => $data['userpassword']));
+	public function getUsername() {
+		return $this->username;
 	}
 
-	# Define a UPDATE Method
-	public function updateUser($id , $data){
-		$this->resource->update('users', array('userpassword' => $data['userpassword']), array('id' => $id));
+	public function setUserpassword($userpassword) {
+		$this->userpassword = $userpassword;
 	}
 
-	# Define a DELETE Method
-	public function deleteUser($id){
-		$this->resource->delete('users', array('id' => $id));
+	public function getUsername() {
+		return $this->userpassword;
 	}
 
+	public function setUser($id, $username, $userpassword) {
+		$this->id = $id;
+		$this->username = $username;
+		$this->userpassword = $userpassword;
+	}
 }
